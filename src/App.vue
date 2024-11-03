ccbbfed9daf713935517759adeb53e0df6140ae6
Cancey-Dejean <you@example.com>
2024-11-03 01:17:41 -0500

    first starter commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue"
+import { Button } from "@/components/ui/button"
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <h1 class="text-4xl font-bold mb-4">Hello World</h1>
+    <Button>Click me</Button>
+  </div>
+</template>
