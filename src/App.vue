4d6ee0e77722afab7b6e69d0d7e7bb293f4563be
Cancey-Dejean <canceyd@gmail.com>
2025-09-18 07:13:33 -0500

    Add components.json configuration, implement Button component, and enhance styling with Tailwind CSS and utility functions; update package dependencies for improved UI components.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import { Button } from "@/components/ui/button";
 </script>
 
 <template>
   <div>
     <h1 class="text-3xl font-bold underline">Hello world!</h1>
-
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <Button>Click me</Button>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped></style>
